a49f816e0dbed832170f6466bcaaa7b0f276af39
Dhiraj123-star <gdhiraj499@gmail.com>
2023-05-14 18:00:44 +0530

    Python fibonacci Series

---
+++ fibonacci_series_1.py
@@ -0,0 +1,14 @@
+# fibonacci series in python
+
+def fibonacci_series(n):
+    x,y=0,1
+    for i in range(n):
+        x,y=0,1
+        print(x)
+
+
+# user input 
+
+number = int(input("Enter your number: "))
+
+fibonacci_series(number)
